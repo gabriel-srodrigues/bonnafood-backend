ad642de9f54347402f72d5f5dde985431820a99a
Gabriel Rodrigues <gabriel.srodrigues.006@gmail.com>
2022-04-17 23:56:42 -0300

    corrigida referencia de user id para segurança desativada

--- src/main/java/br/com/bonnafood/app/products/api/model/ProductDetailedResponse.java
+++
@@ -1,9 +0,0 @@
-package br.com.bonnafood.app.products.api.model;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ProductDetailedResponse {
-}
--- src/main/java/br/com/bonnafood/app/products/domain/filter/ProductFilter.java
+++
@@ -1,9 +0,0 @@
-package br.com.bonnafood.app.products.domain.filter;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ProductFilter {
-}
